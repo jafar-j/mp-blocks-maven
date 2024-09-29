4b74810a62b19e3ac02a70dab2f13fb109fadb76
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2024-09-28 22:57:28 -0500

    cleaned checkstyle on Art80x24

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -6,10 +6,8 @@ import edu.grinnell.csc207.blocks.Grid;
 import edu.grinnell.csc207.blocks.HAlignment;
 import edu.grinnell.csc207.blocks.HComp;
 import edu.grinnell.csc207.blocks.Rect;
-import edu.grinnell.csc207.blocks.Surrounded;
 import edu.grinnell.csc207.blocks.VAlignment;
 import edu.grinnell.csc207.blocks.VComp;
-
 import java.io.PrintWriter;
 
 /**
@@ -22,25 +20,32 @@ public class Art80x24 {
   /**
    * Create the artwork.
    *
-   * @param args
-   *   Command-line arguments (currently ignored).
-   *
-   * @exception Exception
-   *   If something goes wrong with one of the underlying classes.
+   * @param args Command-line arguments (currently ignored).
+   * @exception Exception If something goes wrong with one of the underlying classes.
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    AsciiBlock star = new Boxed(new Rect('*', 1, 1)) ;
-    AsciiBlock flagCorner = new Grid(star, 4,3);
-    AsciiBlock redStarLine = new Grid(new Rect('R',1,1), 56, 3);
-    AsciiBlock whiteStarLine = new Grid(new Rect('W',1,1), 56, 3);
-    AsciiBlock fullRedStarLine = new Grid(new Rect('R',1,1), 80, 3);
-    AsciiBlock fullWhiteStarLine = new Grid(new Rect('W',1,1), 80, 3);
-    AsciiBlock starStripes = new VComp(HAlignment.CENTER, new AsciiBlock[] {redStarLine,whiteStarLine,redStarLine});
-    AsciiBlock starLine = new HComp(VAlignment.TOP, new AsciiBlock[] {flagCorner,starStripes});
-    AsciiBlock lineBlock = new VComp(HAlignment.CENTER, new AsciiBlock[] {fullWhiteStarLine,fullRedStarLine,fullWhiteStarLine,fullRedStarLine,fullWhiteStarLine});
-    AsciiBlock americanFlag = new VComp(HAlignment.CENTER,new AsciiBlock[] {starLine,lineBlock});
+    AsciiBlock star = new Boxed(new Rect('*', 1, 1));
+    AsciiBlock flagCorner = new Grid(star, 4, 3);
+    AsciiBlock redStarLine = new Grid(new Rect('R', 1, 1), 56, 3);
+    AsciiBlock whiteStarLine = new Grid(new Rect('W', 1, 1), 56, 3);
+    AsciiBlock fullRedStarLine = new Grid(new Rect('R', 1, 1), 80, 3);
+    AsciiBlock fullWhiteStarLine = new Grid(new Rect('W', 1, 1), 80, 3);
+    AsciiBlock starStripes =
+        new VComp(HAlignment.CENTER, new AsciiBlock[] {redStarLine, whiteStarLine, redStarLine});
+    AsciiBlock starLine = new HComp(VAlignment.TOP, new AsciiBlock[] {flagCorner, starStripes});
+    AsciiBlock lineBlock =
+        new VComp(
+            HAlignment.CENTER,
+            new AsciiBlock[] {
+              fullWhiteStarLine,
+              fullRedStarLine,
+              fullWhiteStarLine,
+              fullRedStarLine,
+              fullWhiteStarLine
+            });
+    AsciiBlock americanFlag = new VComp(HAlignment.CENTER, new AsciiBlock[] {starLine, lineBlock});
     AsciiBlock.print(pen, americanFlag);
     pen.close();
   } // main(String[])
