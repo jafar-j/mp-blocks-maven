f324f1e2ec43f7da91e9a62664c8d2ce3b3a9ed2
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2024-09-28 22:23:01 -0500

    finished flag art

--- src/main/java/edu/grinnell/csc207/main/Art80x24.java
+++ src/main/java/edu/grinnell/csc207/main/Art80x24.java
@@ -3,8 +3,12 @@ package edu.grinnell.csc207.main;
 import edu.grinnell.csc207.blocks.AsciiBlock;
 import edu.grinnell.csc207.blocks.Boxed;
 import edu.grinnell.csc207.blocks.Grid;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.HComp;
 import edu.grinnell.csc207.blocks.Rect;
 import edu.grinnell.csc207.blocks.Surrounded;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.VComp;
 
 import java.io.PrintWriter;
 
@@ -26,10 +30,18 @@ public class Art80x24 {
    */
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
-    AsciiBlock art = new Rect('^', 80, 24);
+
     AsciiBlock star = new Boxed(new Rect('*', 1, 1)) ;
     AsciiBlock flagCorner = new Grid(star, 4,3);
-    AsciiBlock.print(pen, flagCorner);
+    AsciiBlock redStarLine = new Grid(new Rect('R',1,1), 56, 3);
+    AsciiBlock whiteStarLine = new Grid(new Rect('W',1,1), 56, 3);
+    AsciiBlock fullRedStarLine = new Grid(new Rect('R',1,1), 80, 3);
+    AsciiBlock fullWhiteStarLine = new Grid(new Rect('W',1,1), 80, 3);
+    AsciiBlock starStripes = new VComp(HAlignment.CENTER, new AsciiBlock[] {redStarLine,whiteStarLine,redStarLine});
+    AsciiBlock starLine = new HComp(VAlignment.TOP, new AsciiBlock[] {flagCorner,starStripes});
+    AsciiBlock lineBlock = new VComp(HAlignment.CENTER, new AsciiBlock[] {fullWhiteStarLine,fullRedStarLine,fullWhiteStarLine,fullRedStarLine,fullWhiteStarLine});
+    AsciiBlock americanFlag = new VComp(HAlignment.CENTER,new AsciiBlock[] {starLine,lineBlock});
+    AsciiBlock.print(pen, americanFlag);
     pen.close();
   } // main(String[])
 } // class Art80x24
